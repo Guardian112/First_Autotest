48a1f1dad4710530c2776a68888b7dc9cdb94e61
Guardian <antonzelenkov112@gmail.com>
2023-10-26 13:29:41 +0300

    Version 2

---
+++ src/test/java/AppleTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class AppleTest extends BaseTest {
+    private final static String BASE_URL = "https://appleinsider.ru";
+    private final static String SEARCH_STRING = "Чем отличается iPhone 13 от iPhone 12";
+
+    @Test
+    public void checkHref() {
+
+    }
+}
