55d8f6673f63a11feb42df9120fecac3e90d03d1
Guardian <antonzelenkov112@gmail.com>
2023-10-26 13:11:55 +0300

    Version 1

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,25 @@
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.After;
+import org.junit.Before;
+
+abstract class BaseTest {
+    public void Setup() {
+        WebDriverManager.operadriver().setup();
+        Configuration.browser = "opera";
+        //Configuration.driverManagerEnabled = true;
+        Configuration.browserSize = "1920x1080";
+        Configuration.headless = false;
+    }
+
+    @Before
+    public void init() {
+        Setup();
+    }
+
+    @After
+    public void tearDown() {
+        Selenide.closeWebDriver();
+    }
+}
