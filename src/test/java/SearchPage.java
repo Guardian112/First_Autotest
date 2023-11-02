5e0b6082ad30c6d799a4ce02bc11736d28c6025f
Guardian112 <98177496+Guardian112@users.noreply.github.com>
2023-11-02 12:26:54 +0300

    Update SearchPage.java

--- src/test/java/SearchPage.java
+++ src/test/java/SearchPage.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.ElementsCollection;
+
 import static com.codeborne.selenide.Selenide.$$x;
+
 public class SearchPage {
     private final ElementsCollection articleTitels = $$x("//h2//a");
+    
     /**
      * Возвращает href из первой строки
      */
