9f24414b4eb635b28199d59f9c3f7c9344e7711a
Guardian <antonzelenkov112@gmail.com>
2023-11-02 12:34:39 +0300

    Пытаюсь починить

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,7 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.After;
 import org.junit.Before;
 
-abstract public class BaseTest {
+abstract class BaseTest {
     public void Setup() {
         WebDriverManager.chromedriver().setup();
         Configuration.browser = "chrome";
