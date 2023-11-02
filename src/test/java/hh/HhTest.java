77210ecd758367883fbbf545d57d0c0bc6fbc196
Guardian112 <98177496+Guardian112@users.noreply.github.com>
2023-11-02 12:15:26 +0300

    Delete src/test/java/hh directory

--- src/test/java/hh/HhTest.java
+++
@@ -1,8 +0,0 @@
-package hh;
-
-import core.BaseTest;
-
-public class HhTest extends BaseTest {
-    private final static String URL = "https://kostroma.hh.ru/applicant/resumes/view?resume=67d81a7fff0c8c7fa00039ed1f5972426c6e69";
-
-}
